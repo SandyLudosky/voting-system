f0189c92050c7a0bf16fa1479b500440299904d5
Sandy Instructor <sandyludosky@gmail.com>
2021-03-21 21:44:23 +0100

    Updates

---
+++ precache-manifest.6059be6917df87b8e6f66795f20e0e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcde25e397fa8afa6fe7560812803fb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9aa68f5ed4788469674",
+    "url": "/static/css/main.750f96ca.chunk.css"
+  },
+  {
+    "revision": "df583039f96e55d97a63",
+    "url": "/static/js/2.e80ce7cd.chunk.js"
+  },
+  {
+    "revision": "f9aa68f5ed4788469674",
+    "url": "/static/js/main.566647b4.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
